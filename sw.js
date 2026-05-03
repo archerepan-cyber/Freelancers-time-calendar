5c1ba11f2cb9365f629ca077a98745d0bfc976aa
archerepan-cyber <archerepan@gmail.com>
2026-05-03 12:19:22 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE = 'freelcal-v4';
+const CACHE = 'ftcal-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).then(r => {
