020803f0dc12ed669ec1956d909c2e89f4fa7616
archerepan-cyber <archerepan@gmail.com>
2026-05-12 00:58:30 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mzccal-v3';
+const CACHE = 'mzccal-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
